0ace648a283f4de1ed8366a2609b0a33865a617b
Minna <liu_minna@hotmail.com>
2023-06-26 14:52:55 +0200

    fixes error in StorageStack

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,15 +1,16 @@
-import { Table } from "sst/construcs";
+import { Table } from "sst/constructs";
 
 export function StorageStack({ stack, app }) {
-    const table = new Table(stack, "Notes", {
-        fields: {
-            userId: "string",
-            noteId: "string",
-        },
-        primaryIndex: { partitionKey: "userId", sortKey:"noteId"}
-    });
+  // Create the DynamoDB table
+  const table = new Table(stack, "Notes", {
+    fields: {
+      userId: "string",
+      noteId: "string",
+    },
+    primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+  });
 
-    return {
-        table
-    };
+  return {
+    table,
+  };
 }
