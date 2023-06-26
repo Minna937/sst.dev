ae95a302f6696398fbcbee141787c74e2eeb8e5e
Minna <liu_minna@hotmail.com>
2023-06-26 14:54:24 +0200

    Adding a storage stack

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,16 +1,18 @@
-import { Table } from "sst/constructs";
+import { Bucket, Table } from "sst/constructs";
 
 export function StorageStack({ stack, app }) {
-  // Create the DynamoDB table
-  const table = new Table(stack, "Notes", {
-    fields: {
-      userId: "string",
-      noteId: "string",
-    },
-    primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
-  });
+    const bucket = new Bucket(stack, "Uploads");
+    // Create the DynamoDB table
+    const table = new Table(stack, "Notes", {
+        fields: {
+            userId: "string",
+            noteId: "string",
+        },
+        primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+    });
 
-  return {
-    table,
-  };
+    return {
+        table,
+        bucket,
+    };
 }
