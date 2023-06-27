e148b92ca894754c103ed5c21e5f4e44f2763ce0
Minna <liu_minna@hotmail.com>
2023-06-27 11:08:05 +0200

    update sst

--- packages/functions/src/get.js
+++ packages/functions/src/get.js
@@ -1,22 +1,22 @@
 import { Table } from "sst/node/table";
 import handler from "@notes/core/handler";
-import dynamoDb from "notes/core/dynamodb";
+import dynamoDb from "@notes/core/dynamodb";
 
 export const main = handler(async (event) => {
-    const params = {
-        TabelName: Table.Notes.tableName,
-        // 'Key' defines the partition key and sort key of the item to be retrieved
-        Key: {
-            userId: "123",
-            // The id of the note from the path
-            noteId: event.pathParameters.id
-        }
-    };
+  const params = {
+    TableName: Table.Notes.tableName,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key: {
+      userId: "123", // The id of the author
+      noteId: event.pathParameters.id, // The id of the note from the path
+    },
+  };
 
-    const result = await dynamoDb.get(params);
-    if (!result.Item) {
-        throw new Error("item not found.")
-    };
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
 
-    return result.Item;
+  // Return the retrieved item
+  return result.Item;
 });
