53a2458519678a1fd58a9fb73d3a59b27510bc92
Minna <liu_minna@hotmail.com>
2023-07-07 12:13:58 +0200

    checking nav bar style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,8 +42,11 @@ function App() {
       <div className="App container py-3">
         <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
           <LinkContainer to="/">
-            <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+            <Navbar.Brand className="fw-bold text-muted">
+              Scratch
+            </Navbar.Brand>
           </LinkContainer>
+          
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Nav activeKey={window.location.pathname}>
