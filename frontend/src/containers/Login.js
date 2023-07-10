b1e33d9ebd949324355ed91bd67fc0c652ace281
Minna <liu_minna@hotmail.com>
2023-07-10 17:54:59 +0200

    updates route back to notes

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -5,11 +5,9 @@ import "./Login.css";
 import { Auth } from "aws-amplify";
 import { useAppContext } from "../lib/contextLib";
 import { useFormFields } from "../lib/hooksLib";
-import { useNavigate } from "react-router-dom";
 import { onError } from "../lib/errorLib";
 
 export default function Login() {
-    const nav = useNavigate();
     const { userHasAuthenticated } = useAppContext();
     const [isLoading, setIsLoading] = useState(false);
     const [fields, handleFieldChange] = useFormFields({
@@ -28,7 +26,7 @@ export default function Login() {
         try {
             await Auth.signIn(fields.email, fields.password);
             userHasAuthenticated(true);
-            nav("/");
+  
         } catch (e) {
             onError(e);
             setIsLoading(false);
