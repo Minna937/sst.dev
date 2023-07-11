3e78b94939061ca386abbbe94991d9142b772163
Minna <liu_minna@hotmail.com>
2023-07-11 10:13:38 +0200

    adds ErrorBoundary in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import { AppContext } from "./lib/contextLib";
 import { Auth } from "aws-amplify";
 import { useNavigate } from "react-router-dom";
 import { onError } from "./lib/errorLib";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   const nav = useNavigate();
@@ -24,7 +25,7 @@ function App() {
       userHasAuthenticated(true);
     } catch (e) {
       if (e !== "No current user") {
-      onError(e);
+        onError(e);
       }
     }
 
@@ -67,9 +68,11 @@ function App() {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
-          <Routes />
-        </AppContext.Provider>
+        <ErrorBoundary>
+          <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+            <Routes />
+          </AppContext.Provider>
+        </ErrorBoundary>
       </div>
     )
   );
